5636cba9e76fb7cdc67f165aa8a563027121a380
zIVANtheGREAT <ivancastro99@gmail.com>
2025-07-05 11:38:52 -0700

    This Java program contains simple programs from Chapter 2. Java Fundamentals. the main program just calls a Class that contains Methods to complete the end of chapter Programming challenges.

--- summer.java
+++ summer.java
@@ -10,11 +10,13 @@ public class summer
         return x - y;
     }
 
-    /*This method will display
-    * @param name the name of the person
-    * @param age age of the person
-    * @param salary desired salary
-    * */
+    /**This method will display and return:
+     @param str the name of the person
+     @param age age of the person
+     @param sal desired salary
+     @return A string containing the variables
+     with a sentence.
+     */
 
     static String nameAgeSalary(String str, int age, double sal)
     {
