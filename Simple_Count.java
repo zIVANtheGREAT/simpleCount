ca6c349c0ef434c72e815c6c74dde0e029897abd
zIVANtheGREAT <ivancastro99@gmail.com>
2025-07-05 11:27:09 -0700

    This Java program contains simple programs from Chapter 2. Java Fundamentals. the main program just calls a Class that contains Methods to complete the end of chapter Programming challenges.

---
+++ Simple_Count.java
@@ -0,0 +1,42 @@
+public class Simple_Count
+{
+    static int sum(int x, int y)
+    {
+        return x+y;
+    }
+
+    public static void main(String[] args)
+    {
+        //Declarations
+        String name;
+        int age;
+        double salary;
+
+        //Initialization
+        name = "Larry the Lobster";
+        age = 34;
+        salary = 59345.50;
+
+        System.out.println(summer.nameAgeSalary(name, age, salary));
+
+        System.out.println(sum(3, 4));
+        System.out.println("Hello. Let the code begin.");
+        System.out.println(summer.sum(4, 34));
+        System.out.println(summer.diff(34, 23));
+
+
+        double x = 0.0;
+        int count = 0;
+
+        while(x<2)
+        {
+            count += 1;
+            x += 0.2;
+        }
+
+        System.out.println(x);
+        System.out.println(count);
+
+    }
+
+}
---
+++ summer.java
@@ -0,0 +1,24 @@
+public class summer
+{
+    static int sum(int x, int y)
+    {
+        return x+y;
+    }
+
+    static int diff(int x, int y)
+    {
+        return x - y;
+    }
+
+    /*This method will display
+    * @param name the name of the person
+    * @param age age of the person
+    * @param salary desired salary
+    * */
+
+    static String nameAgeSalary(String str, int age, double sal)
+    {
+        return "My name is " + str + ", my age is " + age +
+                " and I hope to earn $" + sal + " per year";
+    }
+}
